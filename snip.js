c6faa6ae800cb0645394438b304f99047c8d6f46
Jørn Åne de Jong <git@jornane.no>
2015-09-22 10:49:55 +0200

    Various improvements
    Better error handling
    Caching of DNS requests
    Use of a log library, allow detailed logging

--- snip.js
+++ snip.js
@@ -1,48 +1,67 @@
 var sni = require('sni-reader');
 var net = require('net');
 var dns = require('dns');
+var log = require('npmlog')
+var AsyncCache = require('async-cache');
 
 var port = process.env.PORT || 443;
 if (process.env.DNS) {
 	dns.setServers(process.env.DNS.split(','));
 }
 var shutdownGrace = process.env.SHUTDOWN_GRACE || 5000;
+log.level = process.env.LOG_LEVEL || 'info';
+
+var dnsCache = new AsyncCache({
+	max: 1000,
+	maxAge: process.env.DNS_CACHE || 3600 * 1000,
+	load: function (key, cb) {
+		dns.resolve6(key, cb);
+	}
+});
 
 function initSession(serverSocket, sniName) {
-	if (!sniName || sniName.length == 0) {
-		serverSocket.end();
-		return;
-	}
-	dns.resolve6(sniName, function (err, addresses) {
+	dnsCache.get(sniName, function (err, addresses) {
 		if (!addresses || !addresses.length) {
 			serverSocket.end();
-			console.log('Unable to resolve AAAA ' + sniName);
+			log.warn(serverSocket.remoteAddress, 'sni', sniName, 'resolve', err ? err.code : null);
 			return;
 		}
-		ip = addresses[0];
-		clientSocket = new net.Socket({ type: 'tcp6' });
-		clientSocket.connect(443, ip, function () {
+		var ip = addresses[0];
+		var clientSocket = net.connect({port: 443, type: 'tcp6', host: ip});
+		log.verbose(serverSocket.remoteAddress, 'sni', sniName, 'resolved', addresses);
+
+		clientSocket.on('connect', function () {
 			serverSocket.pipe(clientSocket).pipe(serverSocket);
+			log.info(serverSocket.remoteAddress, 'sni', sniName, 'proxied', ip);
 		});
 	});
 }
 
-function interrupt() {
-	server.close();
-	server.getConnections(function (err, count) {
-		if (!err && count) {
-			console.error('Received interrupt signal.');
-			setTimeout(function() {
-				process.exit()
-			}, shutdownGrace)
-		}
-	});
-}
-
-server = net.createServer(function (serverSocket) {
+var server = net.createServer(function (serverSocket) {
 	sni(serverSocket, function(err, sniName) {
-		initSession(serverSocket, sniName);
+		if (err) throw err;
+		if (sniName) {
+			log.verbose(serverSocket.remoteAddress, 'sni', sniName);
+			initSession(serverSocket, sniName);
+		} else {
+			log.warn(serverSocket.remoteAddress, 'sni', '(none)');
+			serverSocket.end();
+		}
 	});
 }).listen(port, '0.0.0.0');
 
 process.once('SIGINT', interrupt);
+function interrupt() {
+	server.close();
+	server.getConnections(function (err, count) {
+		if (!err && count) {
+			console.error('Waiting for clients to disconnect. Grace', shutdownGrace);
+			setTimeout(function() {
+				process.exit();
+			}, shutdownGrace);
+		} else if (err) {
+			console.error('Error while receiving interrupt! Attempt to bail, no grace.', err);
+			process.exit();
+		}
+	});
+}
