fafc10a2194cb43b3d8303b911cf78dddc463c0b
Jørn Åne de Jong <git@jornane.no>
2015-10-14 15:23:29 +0200

    Replace npmlog with log4js

--- snip.js
+++ snip.js
@@ -1,7 +1,8 @@
 var sni = require('sni-reader');
 var net = require('net');
 var dns = require('dns');
-var log = require('npmlog')
+var log4js = require('log4js');
+var logger = log4js.getLogger();
 var AsyncCache = require('async-cache');
 
 var port = process.env.PORT || 443;
@@ -9,13 +10,13 @@ if (process.env.DNS) {
 	dns.setServers(process.env.DNS.split(','));
 }
 var shutdownGrace = process.env.SHUTDOWN_GRACE || 5000;
-log.level = process.env.LOG_LEVEL || 'info';
+logger.setLevel(process.env.LOG_LEVEL || 'INFO');
 
 var dnsCache = new AsyncCache({
 	max: 1000,
 	maxAge: process.env.DNS_CACHE || 3600 * 1000,
 	load: function (key, cb) {
-		log.verbose('dns', 'Looking up AAAA', key);
+		logger.debug('dns', 'Looking up AAAA', key);
 		dns.resolve6(key, cb);
 	}
 });
@@ -24,16 +25,16 @@ function initSession(serverSocket, sniName) {
 	dnsCache.get(sniName, function (err, addresses) {
 		if (err) {
 			serverSocket.end();
-			log.warn('dns', serverSocket.remoteAddress, sniName, 'resolve', err ? err.code : null);
+			logger.warn('dns', serverSocket.remoteAddress, sniName, 'resolve', err ? err.code : null);
 			return;
 		}
 		var ip = addresses[0];
 		var clientSocket = net.connect({port: 443, type: 'tcp6', host: ip});
-		log.silly('tcp', serverSocket.remoteAddress, sniName, 'connecting', addresses);
+		logger.debug('tcp', serverSocket.remoteAddress, sniName, 'connecting', addresses);
 
 		clientSocket.on('connect', function () {
 			serverSocket.pipe(clientSocket).pipe(serverSocket);
-			log.info('tcp', serverSocket.remoteAddress, sniName, 'connected', ip);
+			logger.info('tcp', serverSocket.remoteAddress, sniName, 'connected', ip);
 		});
 	});
 }
@@ -41,21 +42,21 @@ function initSession(serverSocket, sniName) {
 var server = net.createServer(function (serverSocket) {
 	serverSocket.on('error', function(err){
 		if (err.code == 'EPIPE') {
-			log.verbose('sni', serverSocket.remoteAddress, 'Client disconnected before the pipe was connected.');
+			logger.debug('sni', serverSocket.remoteAddress, 'Client disconnected before the pipe was connected.');
 		} else {
-			log.error('sni', err);
+			logger.fatal('sni', err);
 		}
 		serverSocket.end();
 	});
 	sni(serverSocket, function(err, sniName) {
 		if (err) {
-			log.error('sni', err.stack);
+			logger.error('sni', err.stack);
 			serverSocket.end();
 		} else if (sniName) {
-			log.verbose('sni', serverSocket.remoteAddress, sniName);
+			logger.debug('sni', serverSocket.remoteAddress, sniName);
 			initSession(serverSocket, sniName);
 		} else {
-			log.warn('sni', serverSocket.remoteAddress, '(none)');
+			logger.warn('sni', serverSocket.remoteAddress, '(none)');
 			serverSocket.end();
 		}
 	});
@@ -71,7 +72,7 @@ function interrupt() {
 				process.exit();
 			}, shutdownGrace);
 		} else if (err) {
-			console.error('Error while receiving interrupt! Attempt to bail, no grace.', err);
+			console.fatal('Error while receiving interrupt! Attempt to bail, no grace.', err);
 			process.exit();
 		}
 	});
