93a59b03316b9c1b9fe57e691619c0e172393ee6
Jørn Åne de Jong <git@jornane.no>
2015-10-14 15:31:39 +0200

    Use different loggers for different components

--- snip.js
+++ snip.js
@@ -2,7 +2,9 @@ var sni = require('sni-reader');
 var net = require('net');
 var dns = require('dns');
 var log4js = require('log4js');
-var logger = log4js.getLogger();
+var dnsLog = log4js.getLogger('dns');
+var tcpLog = log4js.getLogger('tcp');
+var sniLog = log4js.getLogger('sni');
 var AsyncCache = require('async-cache');
 
 var port = process.env.PORT || 443;
@@ -10,13 +12,15 @@ if (process.env.DNS) {
 	dns.setServers(process.env.DNS.split(','));
 }
 var shutdownGrace = process.env.SHUTDOWN_GRACE || 5000;
-logger.setLevel(process.env.LOG_LEVEL || 'INFO');
+dnsLog.setLevel(process.env.LOG_LEVEL || 'INFO');
+tcpLog.setLevel(process.env.LOG_LEVEL || 'INFO');
+sniLog.setLevel(process.env.LOG_LEVEL || 'INFO');
 
 var dnsCache = new AsyncCache({
 	max: 1000,
 	maxAge: process.env.DNS_CACHE || 3600 * 1000,
 	load: function (key, cb) {
-		logger.debug('dns', 'Looking up AAAA', key);
+		dnsLog.debug('Looking up AAAA', key);
 		dns.resolve6(key, cb);
 	}
 });
@@ -25,16 +29,16 @@ function initSession(serverSocket, sniName) {
 	dnsCache.get(sniName, function (err, addresses) {
 		if (err) {
 			serverSocket.end();
-			logger.warn('dns', serverSocket.remoteAddress, sniName, 'resolve', err ? err.code : null);
+			dnsLog.warn(serverSocket.remoteAddress, sniName, 'resolve', err ? err.code : null);
 			return;
 		}
 		var ip = addresses[0];
 		var clientSocket = net.connect({port: 443, type: 'tcp6', host: ip});
-		logger.debug('tcp', serverSocket.remoteAddress, sniName, 'connecting', addresses);
+		tcpLog.debug(serverSocket.remoteAddress, sniName, 'connecting', addresses);
 
 		clientSocket.on('connect', function () {
 			serverSocket.pipe(clientSocket).pipe(serverSocket);
-			logger.info('tcp', serverSocket.remoteAddress, sniName, 'connected', ip);
+			tcpLog.info(serverSocket.remoteAddress, sniName, 'connected', ip);
 		});
 	});
 }
@@ -42,25 +46,26 @@ function initSession(serverSocket, sniName) {
 var server = net.createServer(function (serverSocket) {
 	serverSocket.on('error', function(err){
 		if (err.code == 'EPIPE') {
-			logger.debug('sni', serverSocket.remoteAddress, 'Client disconnected before the pipe was connected.');
+			sniLog.debug(serverSocket.remoteAddress, 'Client disconnected before the pipe was connected.');
 		} else {
-			logger.fatal('sni', err);
+			sniLog.fatal(err);
 		}
 		serverSocket.end();
 	});
 	sni(serverSocket, function(err, sniName) {
 		if (err) {
-			logger.error('sni', err.stack);
+			sniLog.trace(err);
 			serverSocket.end();
 		} else if (sniName) {
-			logger.debug('sni', serverSocket.remoteAddress, sniName);
+			sniLog.debug(serverSocket.remoteAddress, sniName);
 			initSession(serverSocket, sniName);
 		} else {
-			logger.warn('sni', serverSocket.remoteAddress, '(none)');
+			sniLog.warn(serverSocket.remoteAddress, '(none)');
 			serverSocket.end();
 		}
 	});
 }).listen(port, '0.0.0.0');
+tcpLog.debug('Started listening on tcp4 port ', port);
 
 process.once('SIGINT', interrupt);
 function interrupt() {
